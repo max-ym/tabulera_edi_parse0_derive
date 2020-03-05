1ff16a0842397e09b29bad58d1fe8c1fb9682914
Max Naumchyk <max.naumch@gmail.com>
2020-03-05 23:06:10 +0200

    Final

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,113 @@
 extern crate proc_macro;
+extern crate proc_macro2;
 extern crate syn;
 #[macro_use]
 extern crate quote;
 
-use syn::export::{TokenStream, ToTokens, TokenStreamExt};
+use syn::export::{TokenStream, ToTokens, TokenStreamExt, TokenStream2};
 use syn::parse_macro_input::parse;
-use syn::{ItemStruct};
-use quote::__private::ext::RepToTokensExt;
+use syn::{ItemStruct, ExprParen, TypePath};
 
-#[proc_macro_derive(ParseError, attributes(Error))]
+struct MyField {
+    name: proc_macro2::TokenStream,
+    ty: proc_macro2::TokenStream,
+}
+
+#[proc_macro_derive(ParseError, attributes(ParseDestination))]
 pub fn derive_parse_error(tokens: TokenStream) -> TokenStream {
-    let struc = parse::<ItemStruct>(tokens).unwrap();
-    let error_ty = {
-        let mut name = None;
+    let struc = parse::<ItemStruct>(tokens).expect("Expected struct");
+    let mut result = quote!();
+    let mut fields = Vec::with_capacity(struc.fields.len());
+    let dest = {
+        let mut dest = None;
         for attr in struc.attrs {
-            if attr.path.to_token_stream().to_string() == "Error" {
-                let mut tokens = attr.tokens;
-                let eq = tokens.next().unwrap();
-                if eq.to_string() != "=" {
-                    panic!();
-                }
-                name = Some(eq.to_string());
+            if attr.path.to_token_stream().to_string() == "ParseDestination" {
+                let tokens = attr.tokens;
+                dest = Some({
+                    let mut q = quote!();
+                    let val = parse::<ExprParen>(tokens.into()).unwrap();
+                    q.append_all(val.expr.into_token_stream());
+                    q
+                });
                 break;
             }
         }
-        name.unwrap()
+        dest.unwrap()
     };
-    let mut fields = Vec::with_capacity(struc.fields.len());
-    struct MyField {
-        name: String,
-        ty: String,
-    }
     for field in struc.fields {
+        let ts = field.ty.into_token_stream();
+        let v = parse::<TypePath>(ts.into()).unwrap();
+        let ty = if let syn::PathArguments::AngleBracketed(v)
+                = &v.path.segments.first().unwrap().arguments {
+            v.args.first().unwrap().clone().into_token_stream()
+        } else {
+            unreachable!()
+        };
         fields.push(MyField {
-            ty: field.ty.to_token_stream().to_string(),
-            name: field.ident.unwrap().to_string(),
+            ty,
+            name: field.ident.expect("Expected identifier").to_token_stream(),
         });
     }
     let mut new_fields = Vec::with_capacity(fields.len());
-    for field in fields {
-        let name = field.name;
-        let ty = field.ty;
+    for field in &fields {
+        let name = field.name.to_owned();
+        let ty = field.ty.to_owned();
         new_fields.push({
-                quote!(#name: std::result::Result<#ty, #error_ty>,)
+                quote!(#name: #ty,)
         });
     }
     let mut for_brace_tokens = quote!();
     for field in new_fields {
         for_brace_tokens.append_all(field);
     }
-    let result = quote!(
+    result.append_all(quote!(
         #[derive(Debug)]
-        pub struct ParseResult<'a> { #for_brace_tokens }
-    );
+        pub struct #dest<'a> { #for_brace_tokens }
+    ).into_iter());
+    result.append_all(validate(&fields, dest).into_iter());
     result.into()
 }
 
+fn validate(fields: &Vec<MyField>, dest: TokenStream2) -> proc_macro2::TokenStream {
+    let if_err = {
+        let mut tokens = quote!();
+        for field in fields {
+            let name = &field.name;
+            tokens.append_all(quote!(
+                s.#name.is_err() ||
+            ));
+        }
+        tokens.append_all(quote!(false {
+            Err(ParseError::DataError(self))
+        }));
+        tokens
+    };
+    let else_unwrap_brace = {
+        let ok_brace = {
+            let mut tokens = quote!();
+            for field in fields {
+                let name = &field.name;
+                tokens.append_all(quote!(
+                    #name: self.#name.unwrap(),
+                ));
+            }
+            tokens
+        };
+        let tokens = quote!(Ok(
+            #dest {
+                #ok_brace
+            }
+        ));
+        tokens
+    };
+    quote!(
+        impl<'a> ParseResult<'a> {
+            pub fn validate(self) -> Output<'a> {
+                let s = &self;
+                if #if_err else {
+                    #else_unwrap_brace
+                }
+            }
+        }
+    )
+}
